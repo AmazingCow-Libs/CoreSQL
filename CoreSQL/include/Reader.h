2af5063cc4603431866926fd16bbe8809470dfee
n2omatt <n2omatt@amazingcow.com>
2017-12-12 17:57:00 -0200

    - Add sanity checks.

--- CoreSQL/include/Reader.h
+++ CoreSQL/include/Reader.h
@@ -17,22 +17,25 @@ class Reader
 public:
     int GetInt(int col) const noexcept
     {
+        COREASSERT_ONLY_IN_DEBUG({CheckColumnValidity(col);});
         return sqlite3_column_int(GetABI(), col);
     }
 
-
     double GetDouble(int col) const noexcept
     {
+        COREASSERT_ONLY_IN_DEBUG({CheckColumnValidity(col);});
         return sqlite3_column_double(GetABI(), col);
     }
 
     const void* GetBlob(int col) const noexcept
     {
+        COREASSERT_ONLY_IN_DEBUG({CheckColumnValidity(col);});
         return sqlite3_column_blob(GetABI(), col);
     }
 
     std::string GetText(int col) const noexcept
     {
+        COREASSERT_ONLY_IN_DEBUG({CheckColumnValidity(col);});
         auto str_ptr = reinterpret_cast<const char *>(
             sqlite3_column_text(GetABI(), col)
         );
@@ -41,11 +44,23 @@ public:
     }
 
 
-
     //------------------------------------------------------------------------//
     // Private Methods                                                        //
     //------------------------------------------------------------------------//
 private:
+    void CheckColumnValidity(int col) const
+    {
+        auto column_count = sqlite3_column_count(GetABI());
+        COREASSERT_ASSERT(
+            col >= 0 && col < column_count,
+            "Column (%d) is out of bounds (0,%d]- stmt: (%s)",
+            col,
+            column_count,
+            sqlite3_sql(GetABI())
+        );
+    }
+
+
     sqlite3_stmt* GetABI() const noexcept
     {
         return (static_cast<T const *>(this))->GetABI();
